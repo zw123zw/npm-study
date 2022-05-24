998ff512393b7e70f9f239eeb92e291a9296a633
zw123zw <zw18674069215@163.com>
2022-05-24 23:05:34 +0800

    抽象工厂模式学习

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
 module.exports.name = '3333'
 
 module.exports.age = 123
+
+const as = function () {
+  console.log(as)
+}
--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ var without = _.without([1, 2, 3], 1)
 
 console.log(without)
 console.log(data)
+
+for (let i = 1; i < 10; i++) {
+  console.log(i)
+}
+
+const aa = [1, 2, 3].reduce((prev, next) => {
+  prev[next] = 1
+  return prev
+}, {})
+console.log(aa)
